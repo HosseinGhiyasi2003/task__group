4946abe6f925cbdfc8464dabb24f9be66ce20a74
hossein <hosseinghiyasi2020@gmail.com>
2024-09-09 15:04:18 +0330

    header

---
+++ js/hosseinjs.js
@@ -0,0 +1,12 @@
+const menuIcon = document.querySelector('.nav-menu');
+const bx_plus = document.querySelector('.bx-plus');
+const nav_ul = document.querySelector('.nav-ul');
+
+
+menuIcon.addEventListener('click', ()=> {
+  nav_ul.classList.toggle('show');
+});
+
+menuIcon.addEventListener('click', ()=> {
+  nav_ul.classList.toggle('show');
+})
