9335bc09465b31d5d7877883e79a5cad508ec646
hossein <hosseinghiyasi2020@gmail.com>
2024-09-09 11:10:30 +0330

    accordion

--- js/amir.js
+++ js/amir.js
@@ -1,3 +1,5 @@
+// header
+
 const hamburger = document.querySelector(".hamburger");  
 
 function toggleMenu() {  
@@ -8,3 +10,26 @@ function toggleMenu() {
 
 
 hamburger.addEventListener('click', toggleMenu);
+
+// fag
+let isPlus = true
+const plusIcon = document.querySelectorAll('.faq-plus');
+console.log(plusIcon);
+const myp = document.querySelectorAll('.myp');
+
+plusIcon.forEach((icon,index)=> {
+    icon.addEventListener('click', ()=> {
+        myp[index].classList.toggle('show');
+        if(isPlus) {
+            plusIcon[index].innerHTML = "-";
+            isPlus = false
+        } else {
+            plusIcon[index].innerHTML = "+";
+            isPlus = true
+        }
+    })
+})
+
+
+
+
